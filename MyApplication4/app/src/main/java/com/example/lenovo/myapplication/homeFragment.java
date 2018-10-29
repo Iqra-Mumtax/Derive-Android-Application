723f70bee155bd86528a498185ba70603eb91160
Hassan Zubair <hassanxbot1c@gmail.com>
2018-10-29 22:16:35 +0500

    Errors removed from UI
    Just Signup.java file has errors of unknown type if we exclude the code from signup.java then build is successfull.

--- MyApplication4/app/src/main/java/com/example/lenovo/myapplication/homeFragment.java
+++ MyApplication4/app/src/main/java/com/example/lenovo/myapplication/homeFragment.java
@@ -24,6 +24,8 @@ import java.util.List;
 public class homeFragment extends Fragment {
 
     // TODO: Customize parameter argument names
+    //heelo world
+
     private static final String ARG_COLUMN_COUNT = "column-count";
     // TODO: Customize parameters
     private int mColumnCount = 1;
