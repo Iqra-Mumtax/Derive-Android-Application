d6cdc0a3c8839a34e65e4afa52cc218dd72f39cc
Hassan Zubair <hassanxbot1c@gmail.com>
2018-10-30 10:50:12 +0500

    UI updaed

--- MyApplication4/app/src/main/java/com/example/lenovo/myapplication/signUp.java
+++ MyApplication4/app/src/main/java/com/example/lenovo/myapplication/signUp.java
@@ -8,20 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
+
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,132 +19,12 @@ import java.util.Arrays;
 import java.util.List;
 
 
-public class signUp extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener{
-    private static final String TAG = signUp.class.getSimpleName();
-    private static final int RC_SIGN_IN = 430;
-    private GoogleApiClient mGoogleApiClient;
-    private SignInButton btnSignIn;
-    public LoginButton loginButton;
-    public Button fb, google;
-    public CallbackManager callbackManager;
-    public String id, name, email, gender, birthday;
+public class signUp extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signUp);
-        callbackManager = CallbackManager.Factory.create();
-        fb = (Button) findViewById(R.id.fb);
-        google = (Button) findViewById(R.id.google);
-        loginButton = (LoginButton) findViewById(R.id.login_button);
-        List < String > permissionNeeds = Arrays.asList("user_photos", "email",
-                "user_birthday", "public_profile", "AccessToken");
-        loginButton.registerCallback(callbackManager, new FacebookCallback < LoginResult > () {@Override
-        public void onSuccess(LoginResult loginResult) {
-            System.out.println("onSuccess");
-            String accessToken = loginResult.getAccessToken()
-                    .getToken();
-            Log.i("accessToken", accessToken);
-            GraphRequest request = GraphRequest.newMeRequest(
-                    loginResult.getAccessToken(),
-                    new GraphRequest.GraphJSONObjectCallback() {@Override
-                    public void onCompleted(JSONObject object,
-                                            GraphResponse response) {
-                        Log.i("LoginActivity",
-                                response.toString());
-                        try {
-                            id = object.getString("id");
-                            try {
-                                URL profile_pic = new URL(
-                                        "http://graph.facebook.com/" + id + "/picture?type=large");
-                                Log.i("profile_pic",
-                                        profile_pic + "");
-
-                            } catch (MalformedURLException e) {
-                                e.printStackTrace();
-                            }
-                            Log.e("UserDate", String.valueOf(object));
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    });
-            Bundle parameters = new Bundle();
-            parameters.putString("fields","id,name,email,gender, birthday");
-            request.setParameters(parameters);
-            request.executeAsync();
-        }
-            @Override
-            public void onCancel() {
-                System.out.println("onCancel");
-            }
-            @Override
-            public void onError(FacebookException exception) {
-                System.out.println("onError");
-                Log.v("LoginActivity", exception.getCause().toString());
-            }
-        });
-        initializeGPlusSettings();
-    }
-    private void initializeGPlusSettings(){
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-        btnSignIn.setSize(SignInButton.SIZE_STANDARD);
-        btnSignIn.setScopes(gso.getScopeArray());
-    }
-
-
-
-    private void handleGPlusSignInResult(GoogleSignInResult result) {
-        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-        if (result.isSuccess()) {
-            GoogleSignInAccount acct = result.getSignInAccount();
-            //Fetch values
-            String personName = acct.getDisplayName();
-            String personPhotoUrl = acct.getPhotoUrl().toString();
-            String email = acct.getEmail();
-            String familyName = acct.getFamilyName();
-            Log.e(TAG, "Name: " + personName +", email: " + email + ", Image: " + personPhotoUrl +", Family Name: " + familyName);
-            updateUI(true);
-        } else {
-            updateUI(false);
-        }
-    }
-    @Override
-
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
-        // be available.
-        Log.d(TAG, "onConnectionFailed:" + connectionResult);
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int responseCode,
-                                    Intent data) {
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleGPlusSignInResult(result);
-        }else{
-            super.onActivityResult(requestCode, responseCode, data);
-            callbackManager.onActivityResult(requestCode, responseCode, data);
-        }
-    }
-    public void onClick(View v) {
-        if (v == fb) {
-            loginButton.performClick();
-        }else if(v == google){
-
-        }
-    }
-    private void updateUI(boolean isSignedIn) {
-        if (isSignedIn) {
-            btnSignIn.setVisibility(View.GONE);
-        } else {
-            btnSignIn.setVisibility(View.VISIBLE);
-        }
+        setContentView(R.layout.signup);
     }
 
 }
